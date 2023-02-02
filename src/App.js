29a2476b3077fc4b8e7adbc173bf747fb67ebd34
willl85 <ngowilfried@gmail.com>
2023-02-02 19:56:00 +0100

    Manage events in a react application (home.js)

--- src/App.js
+++ src/App.js
@@ -5,35 +5,14 @@ import Navbar from './Navbar';
 
 function App() {
 
-  const titre = 'turpis massa tincidunt dui ut';
-  const contenu =
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consectetur purus ut faucibus pulvinar elementum integer enim neque volutpat.';
-  const likes = 150;
-  const google = 'https://www.google.com';
-
   return (
     <div className="App">
+
       <Navbar />
       <Home />
-      <br></br>
-      <hr></hr>
-      <br></br>
-      <div className="contenu">
-        <h1>{titre}</h1>
 
-        <p>{contenu}</p>
-
-        <small>{likes} personnes ont aimé ton contenu </small>
-
-        <div>
-          <a href={google} target="_blank">
-            Go to google
-          </a>
-        </div>
-      </div>
     </div>
   );
 }
-
 
 export default App;
--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,28 @@
 const Home = () => {
     const contenu = "Page d'acceuil";
     const colorText = {
-      color: '#00FFFF',
+      color: '#rgba(0,0,0,.9)',
     };
 
+    const HandleClick = () => {
+        console.log('click');
+    }
+
+    const anotherClick = (name, e) => {
+        const home = document.querySelector('.home');
+        const p = document.createElement('p');
+        const el = e.target.localName;
+        p.textContent = `Bonjour ${name} tu as cliqué sur l'element ${el}`;
+
+        home.append(p);
+
+    }
+
     return (
-      <div className="home" style={ colorText }>
+      <div className="home" style={colorText}>
         <h2> {contenu} </h2>
+        <button className="me-2" onClick={ HandleClick }>Cliquer moi!</button>
+        <button className="anotherClick" onClick={ (e) => {anotherClick('Vincent', e)} }>Cliquez encore</button>
       </div>
     );
 }

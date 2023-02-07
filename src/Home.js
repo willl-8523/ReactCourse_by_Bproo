1cf4a132a488abaef44ae40e8a3a31ab02996b7b
willl85 <ngowilfried@gmail.com>
2023-02-07 17:28:32 +0100

    How to pass a function to a props in react

--- src/Home.js
+++ src/Home.js
@@ -2,87 +2,107 @@ import { useState } from 'react'
 import BlogList from './BLogList';
 
 const Home = () => {
-    // const contenu = "Page d'acceuil";
-    // const colorText = {
-    //   color: '#rgba(0,0,0,.9)',
-    // };
+  // const contenu = "Page d'acceuil";
+  // const colorText = {
+  //   color: '#rgba(0,0,0,.9)',
+  // };
 
-    // const HandleClick = () => {
-    //     console.log('click');
-    // }
+  // const HandleClick = () => {
+  //     console.log('click');
+  // }
 
-    // // Declarer une useState ou Hooks
-    // const [lastName, setLastName] = useState('Abouchou');
+  // // Declarer une useState ou Hooks
+  // const [lastName, setLastName] = useState('Abouchou');
 
-    // const anotherClick = (firstName, e) => {
-    //     const home = document.querySelector('.home');
-    //     const p = document.createElement('p');
-    //     const el = e.target.localName;
+  // const anotherClick = (firstName, e) => {
+  //     const home = document.querySelector('.home');
+  //     const p = document.createElement('p');
+  //     const el = e.target.localName;
 
-    //     // Modifier le Hooks pour qu'il soit réactif
-    //     setLastName('Moting');
-        
-    //     // p.textContent = `Bonjour ${firstName} ${lastName} tu as cliqué sur l'element ${el}`;
+  //     // Modifier le Hooks pour qu'il soit réactif
+  //     setLastName('Moting');
 
-    //     // home.append(p);
+  //     // p.textContent = `Bonjour ${firstName} ${lastName} tu as cliqué sur l'element ${el}`;
 
-    // }
+  //     // home.append(p);
 
-    // return (
-    //   <div className="home" style={colorText}>
-    //     <h2> {contenu} </h2>
-    //     <button className="me-2" onClick={HandleClick}>
-    //       Cliquer moi!
-    //     </button>
-    //     <button
-    //       className="anotherClick"
-    //       onClick={(e) => {
-    //         anotherClick('Vincent', e);
-    //       }}
-    //     >
-    //       Cliquez encore
-    //     </button>
-    //     <p>
-    //       Bonjour Vincent {lastName}
-    //     </p>
-    //   </div>
-    // );
+  // }
 
-    // ************************ AFFICHER UNE LISTE ***************************
+  // return (
+  //   <div className="home" style={colorText}>
+  //     <h2> {contenu} </h2>
+  //     <button className="me-2" onClick={HandleClick}>
+  //       Cliquer moi!
+  //     </button>
+  //     <button
+  //       className="anotherClick"
+  //       onClick={(e) => {
+  //         anotherClick('Vincent', e);
+  //       }}
+  //     >
+  //       Cliquez encore
+  //     </button>
+  //     <p>
+  //       Bonjour Vincent {lastName}
+  //     </p>
+  //   </div>
+  // );
 
-    const [blogs, setBLog] = useState([
-      {
-        id: 1,
-        title: 'Lorem ipsum dolor sit ame',
-        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-        author: 'Cheick',
-        date: '07/02/2023',
-      },
-      {
-        id: 2,
-        title: 'Consectetur adipiscing elit',
-        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Congue mauris rhoncus aenean vel elit scelerisque mauris.',
-        author: 'Eget',
-        date: '07/02/2023',
-      },
-      {
-        id: 3,
-        title: 'La nuit des codeurs',
-        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Congue mauris rhoncus aenean vel elit scelerisque mauris.',
-        author: 'Eget',
-        date: '07/02/2023',
-      },
-    ]);
+  // ************************ AFFICHER UNE LISTE ***************************
 
-    return (
-      <div className="home">
-        <BlogList blogs={blogs} title={'Liste des blogs'} />
+  const [blogs, setBLog] = useState([
+    {
+      id: 1,
+      title: 'Lorem ipsum dolor sit ame',
+      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      author: 'Cheick',
+      date: '07/02/2023',
+    },
+    {
+      id: 2,
+      title: 'Consectetur adipiscing elit',
+      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Congue mauris rhoncus aenean vel elit scelerisque mauris.',
+      author: 'Eget',
+      date: '07/02/2023',
+    },
+    {
+      id: 3,
+      title: 'La nuit des codeurs',
+      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Congue mauris rhoncus aenean vel elit scelerisque mauris.',
+      author: 'Eget',
+      date: '07/02/2023',
+    },
+  ]);
 
-        {/* Afficher les articles d'Eget (avec la fction filter de js) */}
-        <BlogList blogs={blogs.filter(blog => blog.author === 'Eget')} title={'Liste des blogs d\'Eget'} />
-      </div>
-    );
+  // return (
+  //   <div className="home">
+  //     <BlogList blogs={blogs} title={'Liste des blogs'} />
 
+  //     {/* Afficher les articles d'Eget (avec la fction filter de js) */}
+  //     <BlogList
+  //       blogs={blogs.filter((blog) => blog.author === 'Eget')}
+  //       title={"Liste des blogs d'Eget"}
+  //     />
+  //   </div>
+  // );
+
+  // ************** AJOUTER UNE FONCTON COMME PROPS EN REACT *****************
+
+  // Supprimer un article uniquement dans le DOM
+  const deleteOneArticle = (id) => {
+    const newBlogs = blogs.filter( (blog) => blog.id !== id);
+    setBLog(newBlogs);
+  }
+  
+  return (
+    <div className="home">
+      <BlogList
+        blogs={blogs}
+        title={'Liste des blogs'}
+        deleteOneArticle={deleteOneArticle}
+      />
+    </div>
+  );
 }
  
 export default Home;
